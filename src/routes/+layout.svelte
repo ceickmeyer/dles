1261038a9ff01f4c0e1de63f969acc14c9054053
Cody Eickmeyer <cody.eickmeyer@gmail.com>
2026-05-12 09:43:46 -0400

    feat: update favicon links and add additional meta tags for better site representation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { data, children } = $props();
 
@@ -19,7 +18,12 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="DLES" />
+	<link rel="manifest" href="/site.webmanifest" />
 	<title>{ogTitle}</title>
 	<meta property="og:site_name" content="The DLES Olympics" />
 	<meta property="og:type" content="website" />
